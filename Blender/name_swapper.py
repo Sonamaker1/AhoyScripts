90b082455ecd4fade27b1486bf687f6244a4fc6e
Winn W. <87080176+Sonamaker1@users.noreply.github.com>
2025-07-09 16:58:49 -0700

    Added button under properties (Press N)

--- Blender/name_swapper.py
+++ Blender/name_swapper.py
@@ -38,17 +38,32 @@ class OUTLINER_OT_swap_names(bpy.types.Operator):
         self.report({'INFO'}, f"Swapped names: {name1} <--> {name2}")
         return {'FINISHED'}
 
+class OUTLINER_PT_swap_names_panel(bpy.types.Panel):
+    bl_label = "Outliner Tools"
+    bl_idname = "OUTLINER_PT_swap_names_panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Item'
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator(OUTLINER_OT_swap_names.bl_idname)
+
+
 def menu_func(self, context):
     if OUTLINER_OT_swap_names.poll(context):
         self.layout.operator(OUTLINER_OT_swap_names.bl_idname, icon='SORTALPHA')
 
 def register():
     bpy.utils.register_class(OUTLINER_OT_swap_names)
+    bpy.utils.register_class(OUTLINER_PT_swap_names_panel)
     bpy.types.OUTLINER_MT_context_menu.append(menu_func)
 
 def unregister():
     bpy.types.OUTLINER_MT_context_menu.remove(menu_func)
+    bpy.utils.unregister_class(OUTLINER_PT_swap_names_panel)
     bpy.utils.unregister_class(OUTLINER_OT_swap_names)
+
 
 if __name__ == "__main__":
     register()
